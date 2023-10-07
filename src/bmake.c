e16ea648bb5b8bde548ad23195458db8ff95e9b8
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-10-07 10:18:45 +0500

    implement the fixes in functions

--- src/bmake.c
+++ src/bmake.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
+#include <string.h>
 #include "functions.h"
 
 int main(int argc, char *argv[]) {
@@ -13,9 +13,29 @@ int main(int argc, char *argv[]) {
     const char *parameter = argv[1];
     const char *sourceFiles = argv[2];
 
+    // Get short and long descriptions interactively
+    char shortDesc[1000];
+    char longDesc[1000];
+    
+    printf("Enter short description: ");
+    if (fgets(shortDesc, sizeof(shortDesc), stdin) == NULL) {
+        printf("Error reading short description.\n");
+        return 1;
+    }
+    shortDesc[strcspn(shortDesc, "\n")] = '\0'; // Remove newline character
+
+    printf("Enter long description (end with a period on its own line):\n");
+    if (fgets(longDesc, sizeof(longDesc), stdin) == NULL) {
+        printf("Error reading long description.\n");
+        return 1;
+    }
+    // Remove trailing newline character
+    longDesc[strcspn(longDesc, "\n")] = '\0';
+
     createGitIgnoreFile();
     createLicenseFile();
-    createManPage(parameter, "Short Description", "Long Description");
+    createManPage(parameter, shortDesc, longDesc);
+    createSpecFile(parameter, shortDesc, longDesc);
     generateMakefile(parameter, sourceFiles);
     generateSourceFiles(parameter, sourceFiles);
 
